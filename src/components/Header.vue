5f536e1bacc9a859bc1609ceaf0f05392cce5466
madnick <madnick@ipap.local>
2024-11-05 03:20:35 +0300

    hel

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,22 +4,12 @@
 
 <template>
   <header class="w-full bg-main flex justify-center">
-      <img src="../assets/video/atTheTable2.gif">
+      <img src="../assets/video/atTheTable2.gif" class="w-full">
   </header>
 </template>
 
 <style scoped>
-.video-background {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-  z-index: -1; /* Помещает видео позади содержимого */
-}
-
-.video-background video {
+video {
   min-width: 100%; /* Убедитесь, что видео покрывает всю область */
   min-height: 100%;
   width: auto;
